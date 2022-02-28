91dc9d0abdd647c556f37f32838b542078936cf7
evroble <emyvroble@gmail.com>
2022-02-28 14:36:29 -0500

    initial commit

---
+++ store/index.js
@@ -0,0 +1,21 @@
+//this is where our reducer will go
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducers({
+    products,
+    subscriptions
+});
+
+//allows us to use chrome devTools to see how our state is being changed.
+//thunkMiddleware lets us dispatch thunk async actions which can allow us to chain calls if needed
+//createLogger logs our redux actions and state
+const middleware = composeWithDevTools(
+    applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+);
+
+const store = createStore(reducer, middleware);
+
+export default store;
---
+++ store/products.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+
+//Action Types:
+const SET_PRODUCTS = 'SET_PRODUCTS';
+
+//Action Creators:
+const setProducts = (products) => {
+    return {
+        type: SET_PRODUCTS,
+        products,
+    };
+};
+
+//Thunks
+export const fetchProducts = () => {
+    return async (dispatch) => {
+        try {
+            const { data } = await axios.get('https://mystifying-spence-dc3bda.netlify.app/build-a-box/products.json');
+            dispatch(setProducts(data))
+        } catch (error) {
+            console.log(error, "Error in fetchProducts")
+        }
+    };
+};
+
+//Reducer
+export default function products(state = [], action){
+    switch(action.type){
+        case SET_PRODUCTS:
+            return action.products;
+        default:
+            return state;
+    }
+}
